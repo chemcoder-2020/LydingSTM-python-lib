aeac2562f7cb53a152e3d758482ec76549ed73ae
Huy <huynguyen.chem@berkeley.edu>
2020-01-17 23:07:56 -0600

    Added function autocrop to myImageToolset

--- myImageToolset.py
+++ myImageToolset.py
@@ -11,7 +11,67 @@ from scipy.ndimage import zoom
 from scipy import fftpack
 import copy
 import pywt
-from sklearn.decomposition import FastICA, KernelPCA, PCA
+from skimage.measure import moments
+from skimage.filters import threshold_otsu, gaussian
+from skimage.color import rgb2gray
+from skimage.morphology import remove_small_objects
+
+# from sklearn.decomposition import FastICA, KernelPCA, PCA
+
+
+def autocrop(img, min_size=30, convert_gray=False, extra_space=0.2):
+    """Separate front ground and background. Choose only largest objects as front ground.
+
+    min_size: minimum size in pixels of objects to include.
+    extra_space: fraction of the front ground size
+    """
+    img = np.array(img).copy()
+    # Convert to grayscale if RGB
+    if len(img.shape) > 2:
+        img_gr = rgb2gray(img)
+    else:
+        img_gr = img.copy()
+
+    # Filter image
+    img_gr = gaussian(img_gr)
+
+    # Convert to binary
+    thr = threshold_otsu(img_gr)
+    binary = img_gr > thr
+
+    # Remove small spots
+    binary = remove_small_objects(binary, min_size=min_size)
+
+    # Find centroid
+    M = moments(binary)
+    centroid = (M[1, 0] / M[0, 0], M[0, 1] / M[0, 0])
+
+    # Find edges
+    fg_indices = np.argwhere(binary > 0)
+    mincol = np.min(fg_indices[:, 1])
+    maxcol = np.max(fg_indices[:, 1])
+    minrow = np.min(fg_indices[:, 0])
+    maxrow = np.max(fg_indices[:, 0])
+    extension = extra_space * np.array([maxrow - minrow, maxcol - mincol])
+
+    # Crop to 100% + extra_space length each dimension
+    framerow = np.clip(
+        [int(minrow - extension[0]), int(maxrow + extension[0])],
+        a_min=0,
+        a_max=img.shape[0],
+    )
+    framecol = np.clip(
+        [int(mincol - extension[1]), int(maxcol + extension[1])],
+        a_min=0,
+        a_max=img.shape[1],
+    )
+
+    if convert_gray:
+        return rgb2gray(
+            img[framerow[0] : framerow[1], framecol[0] : framecol[1]]
+        )
+    else:
+        return img[framerow[0] : framerow[1], framecol[0] : framecol[1]]
 
 
 def destripe_by_wavelet_svd(img, wavelet="sym7", level=None, vecnum=None):
